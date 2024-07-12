08d14b2dd96399707030889fe9dc236ca2c9ff73
shwetanaik08 <112984589+shwetanaik08@users.noreply.github.com>
2024-07-12 14:44:39 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,11 +2,16 @@
 #define STRINGCALCULATOR_H
 
 #include <string>
+#include <vector>
 
 class StringCalculator {
 public:
     int add(const std::string& numbers);
+
+private:
+    // Helper functions
+    std::vector<std::string> split(const std::string& str, const std::string& delimiter);
+    int toInt(const std::string& str);
 };
 
 #endif // STRINGCALCULATOR_H
-
