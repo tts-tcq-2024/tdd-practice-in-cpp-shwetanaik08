41fe5cc30de28299ec5d8e905bd69eb57e3dc65d
shwetanaik08 <112984589+shwetanaik08@users.noreply.github.com>
2024-07-12 15:08:49 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,18 +1,1 @@
-#ifndef STRINGCALCULATOR_H
-#define STRINGCALCULATOR_H
 
-#include <string>
-#include <vector>
-
-#include <gtest/gtest.h>
-class StringCalculator {
-public:
-    int add(const std::string& numbers);
-
-private:
-    // Helper functions
-    std::vector<std::string> split(const std::string& str, const std::string& delimiter);
-    int toInt(const std::string& str);
-};
-
-#endif // STRINGCALCULATOR_H
